0143c05143815bfba2a27e8b67ee8f707f5e9286
tundxraowl <26510970+tundxraowl@users.noreply.github.com>
2023-05-04 14:32:29 -0700

    Go back to text on GPU. Too slow.
    Signed-off-by: bghira <bghira@users.github.com>

--- bark/generation.py
+++ bark/generation.py
@@ -290,7 +290,7 @@ def load_codec_model(use_gpu=True, force_reload=False):
 
 
 def preload_models(
-    text_use_gpu=False,
+    text_use_gpu=True,
     text_use_small=False,
     coarse_use_gpu=True,
     coarse_use_small=False,
